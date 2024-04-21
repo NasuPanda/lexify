14273d22a81a759094a45c16ff077e9d15466a5d
NasuPanda <xiyuanjunzhi@gmail.com>
2024-04-21 22:48:41 +0900

    refactor: Change an arg name

--- backend/app/schemas/card.py
+++ backend/app/schemas/card.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel, Field, Optional
 class CardCreate(BaseModel):
     term: str = Field(..., example="Lexicon")
     definition: str = Field(..., example="The vocabulary of a person, language, or branch of knowledge")
-    examples: Optional[str] = Field(None, example="Her lexicon was vast and filled with unusual words.")
+    example_sentence: Optional[str] = Field(None, example="Her lexicon was vast and filled with unusual words.")
     image_url: Optional[str] = Field(None, example="http://example.com/image.png")
     audio_url: Optional[str] = Field(None, example="http://example.com/audio.mp3")

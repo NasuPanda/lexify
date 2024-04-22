78c3ff536e7baa4dac907a721f8dab4073fa17d4
NasuPanda <xiyuanjunzhi@gmail.com>
2024-04-22 22:18:00 +0900

    refactor: Modify type hint

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,14 +1,26 @@
+from typing import Generator
+
+from app.core.database import SessionLocal
 from app.models.user_model import User
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
 
-def get_current_user(id: int) -> User:
-    # TODO: Implement
-    return User(
-        id=1,
-        username="John Green",
-        password="pumpkin_and_penguins",
-        email="johngreen@example.com",
-    )
 
-def get_db(db_name: str):
-    # TODO: Implement
+# TODO This function is problematic. When it's commented out, it raises the error: NameError: name 'Any' is not defined.
+# def get_current_user(db: Session = Depends(SessionLocal)) -> User:
+#     # TODO # This should be dynamically determined, e.g., from a token
+#     user_id = 1
+#     user = db.query(User).filter(User.id == user_id).first()
+#     if not user:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return user
+
+def get_current_user() -> User:
     return
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

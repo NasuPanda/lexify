c81420a9ee44ad8f5ddcd42581db35ce2adedb7e
NasuPanda <xiyuanjunzhi@gmail.com>
2024-04-24 23:19:01 +0900

    fix: Fix bad arg to inject dependency

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,22 +1,11 @@
 from typing import Generator
 
-from app.core.database import SessionLocal
-from app.models.user_model import User
-from fastapi import Depends, HTTPException
+from fastapi import Depends
 from sqlalchemy.orm import Session
 
+from app.core.database import SessionLocal
+from app.models.user_model import User
 
-# TODO This function is problematic. When it's commented out, it raises the error: NameError: name 'Any' is not defined.
-# def get_current_user(db: Session = Depends(SessionLocal)) -> User:
-#     # TODO # This should be dynamically determined, e.g., from a token
-#     user_id = 1
-#     user = db.query(User).filter(User.id == user_id).first()
-#     if not user:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return user
-
-def get_current_user() -> User:
-    return
 
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
@@ -24,3 +13,8 @@ def get_db() -> Generator[Session, None, None]:
         yield db
     finally:
         db.close()
+
+def get_current_user(db: Session = Depends(get_db)) -> User:
+    # TODO This should be dynamically determined, e.g., from a token
+    user_id = 1
+    return db.query(User).filter(User.id == user_id).first()

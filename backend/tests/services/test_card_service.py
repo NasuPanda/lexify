ba6350166ac7857fade8be39c843425cb6c97e81
NasuPanda <xiyuanjunzhi@gmail.com>
2024-04-25 15:39:24 +0900

    test: add edge cases

--- backend/tests/services/test_card_service.py
+++ backend/tests/services/test_card_service.py
@@ -84,7 +84,21 @@ def test_create_card(mock_db, card_data):
     assert card.definition == card_data.definition
     assert card.example_sentence == card_data.example_sentence
 
+def test_create_card_with_required_fields_only(mock_db):
+    """POST /cards: Card creation with minimal data"""
+    user_id = 1
+    minimum_card_data = CardCreate(
+        term="Minimum",
+        definition="Possible least amount.",
+    )
+    card_with_minimum_data = create_card(mock_db, minimum_card_data, user_id)
+    mock_db.add.assert_called_once_with(card_with_minimum_data)
+    mock_db.commit.assert_called_once()
+    assert card_with_minimum_data.term == minimum_card_data.term
+    assert not card_with_minimum_data.example_sentence
+
 def test_update_card(mock_db, mock_card, updated_card_data):
+    """PUT /cards/{id}: Card update"""
     card_id = 1
     user_id = 1
     mock_card.id = card_id
@@ -97,7 +111,34 @@ def test_update_card(mock_db, mock_card, updated_card_data):
     mock_db.commit.assert_called_once()
     assert card.term == updated_card_data.term
 
+def test_update_card_with_partial_data(mock_db, mock_card):
+    """PUT /cards/{id}: update card with partial data"""
+    unchanged_definition = mock_card.definition
+    unchanged_example_sentence  = mock_card.example_sentence
+
+    card_id = 1
+    user_id = 1
+    mock_card.id = card_id
+    mock_card.user_id = user_id
+    mock_db.query.return_value.filter_by.return_value.first.return_value = mock_card
+
+    card_data_with_term_only = CardUpdate(term="Updated Example")
+    updated_card = update_card(mock_db, card_id, user_id, card_data_with_term_only)
+
+    assert updated_card is mock_card
+    assert updated_card.term == card_data_with_term_only.term
+    assert updated_card.definition == unchanged_definition
+    assert updated_card.example_sentence == unchanged_example_sentence
+
+def test_update_non_existent_card(mock_db, updated_card_data):
+    card_id = 10
+    user_id = 1
+
+    card = update_card(mock_db, card_id, user_id, updated_card_data)
+    assert card is None
+
 def test_delete_card(mock_db, mock_card):
+    """DELETE /cards/{id}: Delete card"""
     card_id = 1
     user_id = 1
     mock_card.id = card_id
